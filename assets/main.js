b430e4decc4be95cf82dcca02be64b481d07488b
Constanza Alennies <constanza.mapp@gmail.com>
2024-11-17 22:59:47 -0300

    funcion suma adicional

--- assets/main.js
+++ assets/main.js
@@ -1,10 +1,16 @@
 const notas =[6,8,9,2,5,10];
-let suma = 0;
 
+// Funciones suma
+const sumando = (a,b,c,d,e,f) => a+b+c+d+e+f;
+console.log ("La suma de las notas según la función con retorno explícito es: " + sumando(6,8,9,2,5,10));
 
-const sumando = () => {
-    suma = suma + notas[0];
-    console.log(suma);
+const sumando_2 = () => {
+    let suma = notas[0]+notas[1]+notas[2]+notas[3]+notas[4]+notas[5]; 
+    return suma  
 }
 
-sumando();
+
+console.log ("La suma de la función con array es: " + sumando_2());
+
+// Funciones promedio
+
